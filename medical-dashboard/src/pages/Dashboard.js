aa50f11d5a9984e87148ddda6f0a20905fb37318
Thesarana Dissanayake <128695643+ThesaranaDissanayake@users.noreply.github.com>
2025-01-08 11:59:37 +0530

    Update Dashboard.js

--- medical-dashboard/src/pages/Dashboard.js
+++ medical-dashboard/src/pages/Dashboard.js
@@ -5,7 +5,7 @@ import Topbar from '../components/Topbar';
 import RecentOrders from '../components/RecentOrders';
 import RecentCustomers from '../components/RecentCustomers';
 import Footer from '../components/Footer';
-import Card from '../components/Card';
+
 
 const Dashboard = () => {
   return (
