4c859c2e753653116817c2eb1a892478318e206b
Thesarana Dissanayake <128695643+ThesaranaDissanayake@users.noreply.github.com>
2025-01-08 11:52:58 +0530

    import health checkup form

--- medical-dashboard/src/pages/HealthMonitor.js
+++ medical-dashboard/src/pages/HealthMonitor.js
@@ -5,6 +5,7 @@ import Topbar from '../components/Topbar';
 import Footer from '../components/Footer';
 import DiseaseUpload from '../components/DiseaseUpload';
 import InformationContainer from '../components/InformationContainer';
+import HealthCheckupForm from '../components/HealthCheckupForm';
 
 const HealthMonitor = () => {
   return (
