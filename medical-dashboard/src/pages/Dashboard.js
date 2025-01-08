64d73cdd42c35d1499efc9be6f9ff55f6a80c4f7
Thesarana Dissanayake <128695643+ThesaranaDissanayake@users.noreply.github.com>
2025-01-08 11:55:35 +0530

    Update Dashboard.js

--- medical-dashboard/src/pages/Dashboard.js
+++ medical-dashboard/src/pages/Dashboard.js
@@ -7,26 +7,6 @@ import RecentCustomers from '../components/RecentCustomers';
 import Footer from '../components/Footer';
 import Card from '../components/Card';
 
-const Dashboard = () => {
-  return (
-    <div className="dashContainer">
-      <Sidebar />
-      <div className="main">
-        <Topbar />
-        <div className="cardBox">
-          <Card numbers="0" cardName="Cattle Management" icon='/icons/farm-icon-removebg-preview.png' />
-          <Card numbers="0 Alerts" cardName="Disease Detection" icon='/icons/cow-icon-removebg-preview.png' />
-          <Card numbers="0 Daily" cardName="Production Management" icon='/icons/milk-icon-removebg-preview.png' />
-          <Card numbers="0 Daily" cardName="Feeding" icon='/icons/feed-icon-removebg-preview.png' />
-        </div>
-        <div className="details">
-          <RecentOrders />
-          <RecentCustomers />
-        </div>
-        <Footer />
-      </div>
-    </div>
-  );
-};
+
 
 export default Dashboard;
