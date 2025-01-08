8d3a8b8d0bee60f87602ef4f6e491179ef4827e0
Thesarana Dissanayake <128695643+ThesaranaDissanayake@users.noreply.github.com>
2025-01-08 11:50:04 +0530

    Update HealthMonitor.js

--- medical-dashboard/src/pages/HealthMonitor.js
+++ medical-dashboard/src/pages/HealthMonitor.js
@@ -7,20 +7,6 @@ import DiseaseUpload from '../components/DiseaseUpload';
 import InformationContainer from '../components/InformationContainer';
 import HealthCheckupForm from '../components/HealthCheckupForm';
 
-const HealthMonitor = () => {
-  return (
-    <div className="dashContainer">
-      <Sidebar />
-      <div className="main">
-        <Topbar />
-        <div className="details">
-          <HealthCheckupForm />
-          <InformationContainer />
-        </div>
-        <Footer />
-      </div>
-    </div>
-  );
-};
+
 
 export default HealthMonitor;
