e94696c05aad1334b2d2d491f6d5b6d4bafcbd66
poornaka <99286090+IT21381218@users.noreply.github.com>
2025-01-13 18:47:34 +0530

    Route for the Dashboard page (accessible after login)

--- medical-dashboard/src/App.js
+++ medical-dashboard/src/App.js
@@ -1,28 +1,39 @@
-// App.js
+// Importing React library
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import './App.css';
-import Home from './pages/Home';
-import SignUp from './pages/forms/SignUp';
-import SignIn from './pages/forms/SignIn';
-import ProfilePictureUpload from './pages/forms/ProfilePictureUpload';
-import DiseaseDetection from './pages/DiseaseDetection';
-import HealthMonitor from './pages/HealthMonitor';
-import MilkProduction from './pages/MilkProduction';
-import VetLocate from './pages/VetLocateMap';
 
+// Importing necessary components from react-router-dom for routing
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Importing custom components/pages
+import Dashboard from './pages/Dashboard';
+import './App.css'; // Importing the CSS file for global styles
+import Home from './pages/Home';
+
+import DiseaseDetection from './pages/DiseaseDetection';
+
+
+// The main App component
 function App() {
   return (
+    // Wrapping the application with the Router component to enable routing
     <Router>
+      {/* Defining routes for the application */}
       <Routes>
+        {/* Route for the Home page */}
         <Route path="/home" element={<Home />} />
+
+        {/* Route for the Dashboard page (accessible after login) */}
         <Route path="/logged/dashboard" element={<Dashboard />} />
+
+        {/* Route for the Disease Detection page */}
         <Route path="/logged/disease-detection" element={<DiseaseDetection />} />
+
+        {/* Catch-all route: If no route matches, the Home page is rendered */}
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
 }
 
+// Exporting the App component as the default export
 export default App;
