33e23263feca141abb002a2e4f8fc4f67f8e11f9
poornaka <99286090+IT21381218@users.noreply.github.com>
2025-01-28 14:37:36 +0530

    udated slidebar and top bar

--- medical-dashboard/src/component/Topbar.js
+++ medical-dashboard/src/component/Topbar.js
@@ -3,17 +3,26 @@ import React from 'react';
 const Topbar = () => {
   return (
     <div className="topbar">
+      {/* Toggle Menu */}
       <div className="toggle">
-        <ion-icon name="menu-outline"></ion-icon>
+        <ion-icon name="menu-outline" role="img" aria-label="Toggle menu"></ion-icon>
       </div>
+
+      {/* Search Bar */}
       <div className="search">
         <label>
-          <input type="text" placeholder="Search here" />
-          <ion-icon name="search-outline"></ion-icon>
+          <input
+            type="text"
+            placeholder="Search here"
+            aria-label="Search input"
+          />
+          <ion-icon name="search-outline" role="img" aria-label="Search icon"></ion-icon>
         </label>
       </div>
+
+      {/* User Profile */}
       <div className="user">
-        <img src="assets/imgs/customer01.jpg" alt="user" />
+        <img src="assets/imgs/customer01.jpg" alt="User Profile" />
       </div>
     </div>
   );
