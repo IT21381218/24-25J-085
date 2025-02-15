5291642e4fc2328a7b401a93a4dbe410a18bf01c
Thesarana Dissanayake <128695643+ThesaranaDissanayake@users.noreply.github.com>
2025-02-15 16:18:26 +0530

    import new sidebar

--- medical-dashboard/src/pages/DiseaseDetection.js
+++ medical-dashboard/src/pages/DiseaseDetection.js
@@ -1,6 +1,5 @@
 // pages/DiseaseDetection.js
 import React from 'react';
-import Sidebar from '../components/Sidebar';
 import Topbar from '../components/Topbar';
 import Footer from '../components/Footer';
 import DiseaseUpload from '../components/DiseaseUpload';
