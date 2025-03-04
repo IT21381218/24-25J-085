138b5f767859e8dcd92469f598140da20e25c082
Eehanee Hettiarachchi <it21379956@my.sliit.lk>
2025-03-04 20:07:13 +0530

    Locate vet

--- medical-dashboard/src/pages/VetLocateMap.js
+++ medical-dashboard/src/pages/VetLocateMap.js
@@ -1,17 +1,43 @@
+// // pages/VetLocate.js
+// import React from 'react';
+// import Sidebar from '../components/Sidebar';
+// import Topbar from '../components/Topbar';
+// import Footer from '../components/Footer';
+// import MilkQualityCheckForm from '../components/MilkQualityCheckForm';
+// import CustomMap from '../components/CustomMap';
+// import InformationContainer from '../components/InformationContainer';
+
+// const VetLocate = () => {
+//   return (
+//     <div className="dashContainer">
+//       <Sidebar />
+//       <div className="main">
+//         <Topbar />
+//         <div className="details">
+//           <CustomMap />
+//           <InformationContainer />
+//         </div>
+//         <Footer />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default VetLocate;
+
 // pages/VetLocate.js
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import Topbar from '../components/Topbar';
 import Footer from '../components/Footer';
-import MilkQualityCheckForm from '../components/MilkQualityCheckForm';
 import CustomMap from '../components/CustomMap';
 import InformationContainer from '../components/InformationContainer';
 
 const VetLocate = () => {
   return (
-    <div className="dashContainer">
+    <div className="dash-container">
       <Sidebar />
-      <div className="main">
+      <div className="main-content">
         <Topbar />
         <div className="details">
           <CustomMap />
