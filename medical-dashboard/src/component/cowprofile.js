9dec84d1a2308e1f7118831bf0ebf8c5e231408f
Thesarana Dissanayake <128695643+ThesaranaDissanayake@users.noreply.github.com>
2025-03-14 22:46:31 +0530

    Image Upload code  set

--- medical-dashboard/src/component/cowprofile.js
+++ medical-dashboard/src/component/cowprofile.js
@@ -60,11 +60,11 @@ const CowProfile = () => {
       const handleImageUpload = (e) => {
         const file = e.target.files[0];
         if (file) {
-            const reader = new FileReader();
-            reader.onloadend = () => setImagePreview(reader.result);
-            reader.readAsDataURL(file);
-
-      }
+          const reader = new FileReader();
+          reader.onloadend = () => setImagePreview(reader.result);
+          reader.readAsDataURL(file);
+        }
+      };
       
       
 
