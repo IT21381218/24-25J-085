b0f955da5766faf24d687ccc21c91c9fa2a34adf
Eehanee Hettiarachchi <it21379956@my.sliit.lk>
2025-03-14 23:05:44 +0530

    VetLocateMaop update

--- medical-dashboard/src/pages/VetLocateMap.js
+++ medical-dashboard/src/pages/VetLocateMap.js
@@ -1,93 +1,29 @@
-// // pages/VetLocate.js
-// import React from 'react';
-// import Sidebar from '../components/Sidebar';
-// import Topbar from '../components/Topbar';
-// import Footer from '../components/Footer';
-// import MilkQualityCheckForm from '../components/MilkQualityCheckForm';
-// import CustomMap from '../components/CustomMap';
-// import InformationContainer from '../components/InformationContainer';
-
-// const VetLocate = () => {
-//   return (
-//     <div className="dashContainer">
-//       <Sidebar />
-//       <div className="main">
-//         <Topbar />
-//         <div className="details">
-//           <CustomMap />
-//           <InformationContainer />
-//         </div>
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VetLocate;
-
-
-
-
-
-
-
 // pages/VetLocate.js
-// import React from 'react';
-// import Sidebar from '../components/Sidebar';
-// import Topbar from '../components/Topbar';
-// import Footer from '../components/Footer';
-// import CustomMap from '../components/CustomMap';
-// import InformationContainer from '../components/InformationContainer';
-
-// const VetLocate = () => {
-//   return (
-//     <div className="dash-container">
-//       <Sidebar />
-//       <div className="main-content">
-//         <Topbar />
-//         <div className="details">
-//           <CustomMap />
-//           <InformationContainer />
-//         </div>
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VetLocate;
-
-
-
-
-
-
-
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import Topbar from '../components/Topbar';
 import Footer from '../components/Footer';
+import MilkQualityCheckForm from '../components/MilkQualityCheckForm';
 import CustomMap from '../components/CustomMap';
 import InformationContainer from '../components/InformationContainer';
 
-const VetLocateLayout = ({ children }) => (
-  <div className="dash-container">
-    <Sidebar />
-    <div className="main-content">
-      <Topbar />
-      {children}
-      <Footer />
+const VetLocate = () => {
+  return (
+    <div className="dashContainer">
+      <Sidebar />
+      <div className="main">
+        <Topbar />
+        <div className="details">
+          <CustomMap />
+          <InformationContainer />
+        </div>
+        <Footer />
+      </div>
     </div>
-  </div>
-);
-
-const VetLocate = () => (
-  <VetLocateLayout>
-    <div className="details">
-      <CustomMap />
-      <InformationContainer />
-    </div>
-  </VetLocateLayout>
-);
+  );
+};
 
 export default VetLocate;
+
+
+
