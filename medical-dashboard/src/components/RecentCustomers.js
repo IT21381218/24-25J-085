cfc82567aa49f5f42922d648d92481f084c7b8bc
Eehanee Hettiarachchi <it21379956@my.sliit.lk>
2025-03-15 14:22:31 +0530

    Update recent customers

--- medical-dashboard/src/components/RecentCustomers.js
+++ medical-dashboard/src/components/RecentCustomers.js
@@ -1,13 +1,42 @@
+// // components/RecentCustomers.js
+// import React from 'react';
+
+// const RecentCustomers = () => {
+//   return (
+//     <div className="recentCustomers">
+//       <div className="cardHeader">
+//         <h2>Notifications</h2>
+//       </div>
+      
+//     </div>
+//   );
+// };
+
+// export default RecentCustomers;
+
+
+
+
 // components/RecentCustomers.js
 import React from 'react';
 
-const RecentCustomers = () => {
+const RecentCustomers = ({ customers }) => {
   return (
     <div className="recentCustomers">
       <div className="cardHeader">
         <h2>Notifications</h2>
       </div>
-      
+      <ul className="customer-list">
+        {customers.map((customer, index) => (
+          <li key={index} className="customer-item">
+            <img src={customer.image} alt={customer.name} className="customer-image" />
+            <div className="customer-info">
+              <h4>{customer.name}</h4>
+              <p>{customer.notification}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
