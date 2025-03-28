1b8fa64e122e210815a30a340e6584fe4f1361f2
poornaka <99286090+IT21381218@users.noreply.github.com>
2025-03-28 15:16:59 +0530

    data with disease and .env files

---
+++ medical-dashboard/src/data/Env.js
@@ -0,0 +1,10 @@
+const ENV = {
+    SERVER: 'http://localhost:8000',
+    PORT: '8000',
+    SOCKET_SERVER: 'http://localhost:6379',
+
+}
+
+// https://cattle-y-backend.onrender.com
+
+export default ENV
